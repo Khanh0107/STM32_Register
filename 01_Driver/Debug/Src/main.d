1dac745368e012673d8cfc4e11103c6842090c47
Khanh <nguyenduykhanhute@gmail.com>
2025-11-11 21:48:06 +0700

    first commit drivere on off led GPIO

---
+++ 01_Driver/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32f7.h \
+ C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32_gpio.h \
+ C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32f7.h
+C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32f7.h:
+C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32_gpio.h:
+C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32f7.h:
---
+++ 01_Driver/Debug/drivers/Src/stm32_gpio.d
@@ -0,0 +1,5 @@
+drivers/Src/stm32_gpio.o: ../drivers/Src/stm32_gpio.c \
+ C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32_gpio.h \
+ C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32f7.h
+C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32_gpio.h:
+C:/Embedded/STM/Workspace/Driver/drivers/Inc/stm32f7.h:
